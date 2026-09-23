b4b4448307d8c9f95b65dee2bc481ba6eeed0023
kongjjj <kongjjj@gmail.com>
2026-09-23 11:03:27 +0800

    UI update FloatingBubble

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "0.0.4"
+        versionName = "0.0.4a"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
